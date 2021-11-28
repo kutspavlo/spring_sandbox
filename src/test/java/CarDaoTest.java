ce8cd2abc9b629b9e88293f74affd6ee1acbb27d
admin <admin@MacBook-Pro-admin.local>
2021-11-29 00:06:30 +0200

    test implementation

--- src/test/java/CarDaoTest.java
+++ src/test/java/CarDaoTest.java
@@ -1,21 +1,27 @@
 import spring.practice.models.Car;
 import spring.practice.dao.CarDAO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarDaoTest implements CarDAO {
+    private ArrayList<Car> cars;
+    public CarDaoTest(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
+
     @Override
-    public List<Car> getAll() {
-        return null;
+    public ArrayList<Car> getAll() {
+        return cars;
     }
 
     @Override
     public void createCar(Car car) {
-
+        cars.add(car);
     }
 
     @Override
     public void deleteCar(Car car) {
-
+        cars.remove(car);
     }
 }
